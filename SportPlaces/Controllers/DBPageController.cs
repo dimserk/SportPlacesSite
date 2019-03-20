af65c9f8d22f1b0f9715d3982ab256c7d93af067
dimserk <43451705+dimserk@users.noreply.github.com>
2019-03-20 18:54:24 +0300

    Создал каркас страницы бд

--- SportPlaces/Controllers/DBPageController.cs
+++ SportPlaces/Controllers/DBPageController.cs
@@ -3,13 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SportPlaces.Models;
 
 namespace SportPlaces.Controllers
 {
     public class DBPageController : Controller
     {
+        private readonly EntitiesContext _context;
+
+        public DBPageController(EntitiesContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
+            ViewBag.CitiesCount = _context.Cities.Count();
+            ViewBag.PhotosCount = _context.Photos.Count();
+            ViewBag.SportKindsCount = _context.SportKinds.Count();
+            ViewBag.SportObjectsCount = _context.SportObjects.Count();
+            ViewBag.UsersCount = _context.Users.Count();
+            ViewBag.RecordsCount = _context.Records.Count();
+                 
             return View();
         }
     }
